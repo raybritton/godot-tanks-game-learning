69301e15cc854018e1ce90197d727453a36ce0eb
Ray Britton <raybritton@gmail.com>
2020-06-05 16:36:01 +0100

    Fix brick collision properly

--- World/Brick.gd
+++ World/Brick.gd
@@ -1,4 +1,6 @@
 extends StaticBody2D
 
-func _on_Hurtbox_area_entered(_area):
-	queue_free()
+func _on_Hurtbox_area_entered(area):
+	if !area.hasInteracted:
+		area.hasInteracted = true
+		queue_free()
